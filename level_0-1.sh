3807c94c670ede2788cf5d63d075075be67780ee
inura-perera <inuraperera6@gmail.com>
2024-05-27 19:11:55 +0530

    Add solutions for OverTheWire Bandit level 0->1

---
+++ level_0-1.sh
@@ -0,0 +1,26 @@
+# The password for the next level is stored in a file called readme located in the home directory. 
+# Use this password to log into bandit1 using SSH. 
+# Whenever you find a password for a level, use SSH (on port 2220) to log into that level and continue the game.
+
+
+# Solution
+
+# lets see the available files in the directory
+
+ls 
+
+# lets read the content of the file readme
+
+cat readme
+
+# The password for the next level is there, copy it.
+
+# Exit to the shell
+
+exit
+
+# lets save the password in a file.
+
+echo  PASSWORD > 1
+
+# Replace PASSWORD with the password you copied.
