9925cf61e1f0ebc7deb264938728e26744251510
inura-perera <inuraperera6@gmail.com>
2024-05-28 19:27:44 +0530

    Add solutions for OverTheWire Bandit level 1->2

---
+++ level_1-2.sh
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+# solution
+
+# log into level 2
+
+sshpass -p $(cat 1) ssh bandit0@bandit.labs.overthewire.org -p 2220
+
+# read the content of the file -
+
+cat ./-
+
+# the password is there, copy it
+
+exit
+
+# save the password in a file
+
+echo PASSWORD > 2
+
